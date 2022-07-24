1e6267d13f484ad733d3f5bcc748f837a6bb3f47
tlgsoft <10814126+tlgsoft@users.noreply.github.com>
2022-07-24 16:10:49 +0100

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 def entry():
   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes)
 
 if __name__== "__main__":
   entry()
